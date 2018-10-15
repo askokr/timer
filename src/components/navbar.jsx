1bd96053c5639dd46056bebf55edc532d2476262
Asko Kriiska <asko.kriiska@gmail.com>
2018-10-15 15:33:17 +0300

    media query

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 import Clock from "./NavBarElements/clock";
 import SaveLoadButtonGruop from "./NavBarElements/saveLoadButtonGroup";
 import SortOrderButtonGroup from "./NavBarElements/sortOrderButtonGroup";
@@ -17,28 +18,62 @@ const NavBar = ({
 }) => {
   let isThereACookie = document.cookie !== "" ? true : false;
   return (
-    <nav className="navbar sticky-top navbar-light bg-dark">
-      <div className="row">
-        {/* <h1 className="text-white m-4">Events</h1> */}
+    <React.Fragment>
+      <MediaQuery minDeviceWidth={1224}>
+        <nav className="navbar sticky-top navbar-light bg-dark">
+          <div className="row">
+            {/* <h1 className="text-white m-4">Events</h1> */}
 
-        <SaveLoadButtonGruop
-          onDelete={onDelete}
-          onReadCookie={onReadCookie}
-          onWriteCookie={onWriteCookie}
-          isThereACookie={isThereACookie}
-        />
+            <SaveLoadButtonGruop
+              onDelete={onDelete}
+              onReadCookie={onReadCookie}
+              onWriteCookie={onWriteCookie}
+              isThereACookie={isThereACookie}
+            />
 
-        <DeleteAllButtonGroup onDelete={onDelete} />
+            <DeleteAllButtonGroup onDelete={onDelete} />
 
-        <SortOrderButtonGroup onSort={onSort} sortDirection={sortDirection} />
+            <SortOrderButtonGroup
+              onSort={onSort}
+              sortDirection={sortDirection}
+            />
 
-        <ElementsToDisplayButtonGroup
-          onDisplay={onDisplay}
-          whatEvetsToDisplay={whatEvetsToDisplay}
-        />
-      </div>
-      <Clock time={time} />
-    </nav>
+            <ElementsToDisplayButtonGroup
+              onDisplay={onDisplay}
+              whatEvetsToDisplay={whatEvetsToDisplay}
+            />
+          </div>
+          <Clock time={time} />
+        </nav>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={1224}>
+        <nav className="navbar navbar-light bg-dark">
+          <div className="row">
+            {/* <h1 className="text-white m-4">Events</h1> */}
+
+            <SaveLoadButtonGruop
+              onDelete={onDelete}
+              onReadCookie={onReadCookie}
+              onWriteCookie={onWriteCookie}
+              isThereACookie={isThereACookie}
+            />
+
+            <DeleteAllButtonGroup onDelete={onDelete} />
+
+            <SortOrderButtonGroup
+              onSort={onSort}
+              sortDirection={sortDirection}
+            />
+
+            <ElementsToDisplayButtonGroup
+              onDisplay={onDisplay}
+              whatEvetsToDisplay={whatEvetsToDisplay}
+            />
+          </div>
+          <Clock time={time} />
+        </nav>
+      </MediaQuery>
+    </React.Fragment>
   );
 };
 
